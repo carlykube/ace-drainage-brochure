83e69aef495087ef3d52902bd79cc90c6622dc7a
carlykube <car.mar.kuba@gmail.com>
2022-07-03 01:26:04 -0500

    Remove link from header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,24 +1,21 @@
 import styles from './Header.module.css';
-import Link from 'next/Link';
 
 const Header = () => {
     return (
         <header className={styles.container}>
-            <Link href="/">
-                <div className={styles.linkContent}>
-                    <img
-                        alt=""
-                        src="/logo-filled-white-2.png"
-                        className={styles.logo}
-                    />
-                    <div className={styles.linkText}>
-                        <div className={styles.linkTextMain}>Ace</div>
-                        <div className={styles.linkTextSecondary}>
-                            Drainage Company
-                        </div>
+            <div className={styles.linkContent}>
+                <img
+                    alt=""
+                    src="/logo-filled-white-2.png"
+                    className={styles.logo}
+                />
+                <div className={styles.linkText}>
+                    <div className={styles.linkTextMain}>Ace</div>
+                    <div className={styles.linkTextSecondary}>
+                        Drainage Company
                     </div>
                 </div>
-            </Link>
+            </div>
         </header>
     );
 };
